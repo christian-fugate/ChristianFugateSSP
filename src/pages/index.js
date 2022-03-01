debfcdf298f8e8338a3acb6c81ef023aae20c1e3
christian-fugate <christian.fugate2@outlook.com>
2022-02-28 21:39:43 -0500

    Added layout file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
-// Step 1: Import React
-import * as React from 'react'
-import { Link } from 'gatsby'
 
-// Step 2: Define your component
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+        <p>I'm making this by following the gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
-// Step 3: Export your component
 export default IndexPage
