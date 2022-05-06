3cf6440d73a0a0f2e2e4c3130e1432c6747e5a26
christian-fugate <christian.fugate2@outlook.com>
2022-05-06 14:00:19 -0400

    changes to gatsbyConfic

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,6 @@ module.exports = {
     title: "Pokedex",
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog/`,
-      },
-    },
-    "gatsby-plugin-mdx",
+    
   ]
 };
